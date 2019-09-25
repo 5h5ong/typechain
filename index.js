542c4d293b89034cfec4792f23d88699ffe28927
5h5ong <Call5h5ong@gmail.com>
2019-09-25 23:57:58 +0900

    #4 Types in Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('hello!!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const age = '20', name = 'shsong', gender = 'male';
+const sayHi = (age, name, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(age, name, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hello!!');
+const age = '20',
+  name = 'shsong',
+  gender = 'male';
+
+const sayHi = (age, name?, gender?) => {
+  console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(age);
+
+// feature somthing
+export {};
