ef104390784edb9914ef5450f84416657ea4ec25
5h5ong <Call5h5ong@gmail.com>
2019-09-27 19:20:01 +0900

    #6 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-interface Human {
-  age: number;
-  name: string;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-const person = {
-  age: 20,
-  name: 'shsong',
-  gender: 'male'
-};
+const shsong = new Human('shsong', 20, 'male');
 const sayHi = (person: Human): string => {
   const { name, age, gender } = person;
   return `hello ${name}, you are ${age}, you are a ${gender}`;
 };
-console.log(sayHi(person));
+console.log(sayHi(shsong));
 
 // feature somthing
 export {};
