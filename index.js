5298cc081824d0b99c59a93f61766ae3fb8a11c5
5h5ong <Call5h5ong@gmail.com>
2019-09-27 17:04:22 +0900

    change basic structure

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const age = '20', name = 'shsong', gender = 'male';
-const sayHi = (age, name, gender) => {
-    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(age, name, gender);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (age: number, name: string, gender: string): string => {
+  return `hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi(30, 'shsong', 'male'));
+
+// feature somthing
+export {};
