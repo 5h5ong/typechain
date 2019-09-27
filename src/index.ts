131e9a3eb3c1e09fc3834e0526d380d5e1c15dce
5h5ong <Call5h5ong@gmail.com>
2019-09-27 17:18:39 +0900

    install typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const sayHi = (age: number, name: string, gender: string): string => {
   return `hello ${name}, you are ${age}, you are a ${gender}`;
 };
-console.log(sayHi(30, 'shsong', 'male'));
+console.log(sayHi(20, 'shsong', 'male'));
 
 // feature somthing
 export {};
